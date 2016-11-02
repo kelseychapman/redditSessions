b4d5dd2e4cb50e5de4e4c5a26502193e0971a254
James Freeman <freeman.james.h@gmail.com>
2016-11-02 17:35:11 -0600

    add controller logic

--- public/javascripts/controllers.js
+++ public/javascripts/controllers.js
@@ -1,3 +1,53 @@
 app.controller('main', function($scope){
-  
+
+  $scope.arr = [
+    {
+      title: 'A title',
+      comments: [1,2,3],
+      author: 'James',
+      showCommentsBool: false,
+      voteCount: 2
+    },
+    {
+      title:'B Title ',
+      comments: [1],
+      author: 'Sasha',
+      showCommentsBool: false,
+      voteCount: 2
+    }
+  ]
+
+
+  $scope.sortType = '-voteCount'
+  $scope.sortDisplay = "Votes"
+
+  $scope.increment = function(post){
+    post.voteCount++
+  }
+
+  $scope.decrement = function(post){
+    post.voteCount--
+  }
+
+  $scope.toggleComments = function(post){
+    if (post.showCommentsBool){
+      post.showCommentsBool = false
+    } else {
+      post.showCommentsBool = true
+    }
+  }
+
+
+
+  $scope.sortBy = function(input){
+    console.log('sortBy clicked');
+    $scope.sortType = input;
+    if (input == '-voteCount') {
+      $scope.sortDisplay = 'Votes'
+    } else {
+      $scope.sortDisplay = 'Title'
+    }
+  }
+
+
 })
