6ed4cf7639810c1f2830974420ff52780d7ea6a7
James Freeman <freeman.james.h@gmail.com>
2016-11-03 10:20:45 -0600

    posts show user using innerjoin

--- routes/api.js
+++ routes/api.js
@@ -4,9 +4,12 @@ var knex = require('../db/knex.js')
 
 /* GET home page. */
 router.post('/allposts', function(req, res, next) {
-  knex('posts').then(function(results){
-    res.json(results)
-  })
+  // knex('posts').then(function(results){
+  //   res.json(results)
+  knex('posts').innerJoin('users', 'posts.user_id', 'users.id')
+    .then(function(results) {
+      res.json(results)
+    })
 });
 
 module.exports = router;
